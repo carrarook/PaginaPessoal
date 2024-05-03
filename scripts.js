00df55ec4102670ed714a8e78bd6e34230476f80
carrarook <obrunocarraro@gmail.com>
2024-05-02 23:01:06 -0300

    Add js do forms para salvar infos

--- scripts.js
+++ scripts.js
@@ -6,3 +6,23 @@ const tween = KUTE.fromTo(
     { path: '#blob2' },
     { repeat: 999, duration: 3000, yoyo: true }
   ).start();
+  
+  function createJson(event) {
+    event.preventDefault(); 
+    let nome = document.getElementById("Nome").value;
+    let email = document.getElementById("Email").value;
+    let telefone = document.getElementById("Telefone").value;
+    
+    let Contato = {
+      Nome: nome,
+      Email: email,
+      Telefone: telefone
+    };
+
+    localStorage.setItem("Contato", JSON.stringify(Contato));
+    
+    console.log(JSON.parse(localStorage.getItem("Contato")));
+
+    document.getElementById("formContato").reset();
+
+  }
