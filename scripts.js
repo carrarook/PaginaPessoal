f9a2fb2a42af18bafc6c43c9d25a07d7cbe02f01
carrarook <obrunocarraro@gmail.com>
2024-05-17 21:59:39 -0300

    Adicionando info via API + iniciando a inserir informação visual

--- scripts.js
+++ scripts.js
@@ -42,21 +42,56 @@ const tween = KUTE.fromTo(
   }
 
   // Get request teste
+  const url = 'https://api.github.com/users/carrarook/repos';
 
-  const url = 'https://api.github.com/users/carrarook/repos'
-  async function fetchData() {
+async function listRepo() {
     try {
         const response = await fetch(url);
         if (!response.ok) {
             throw new Error('Erro na solicitação: ' + response.statusText);
         }
-
-        // Converte pra JSON
         const repos = await response.json();
         
-        console.log(repos);
+        const filteredRepos = repos.map(repo => ({
+            id: repo.id,
+            name: repo.name,
+            link: repo.html_url,
+            description: repo.description
+        }));
+
+        createCarrousel(filteredRepos); // Chama a função createCarrousel com os repositórios filtrados
     } catch (error) {
         console.error('Erro:', error);
     }
 }
-listRepo()
+
+function createCarrousel(repos) {
+    const cardContainer = document.getElementById('cardContainer');
+
+    // Limpa o conteúdo atual do container
+    cardContainer.innerHTML = '';
+
+    // Itera sobre o array de repositórios
+    repos.forEach(repo => {
+        // Cria um novo elemento div para o card
+        const card = document.createElement('div');
+        card.classList.add('cardRepo-card');
+        card.style.backgroundColor = '#02bcf5'; // Cor de fundo do card, você pode ajustar conforme necessário
+
+        // Cria o conteúdo do card com base nos dados do objeto
+        card.innerHTML = `
+            <h3><a href="${repo.link}" target="_blank">${repo.name}</a></h3>
+            <p class="cardRepo-card--content">${repo.description}</p>
+            <p class="cardRepo-card--link"><a href="${repo.link}" target="_blank">Learn more <svg class="HoverArrow" width="10" height="10" viewBox="0 0 10 10" aria-hidden="true"><g fill-rule="evenodd"><path class="HoverArrow__linePath" d="M0 5h7"></path><path class="HoverArrow__tipPath" d="M1 1l4 4-4 4"></path></g></svg></a></p>
+        `;
+        console.log(repo)
+        // Adiciona o card ao container
+        cardContainer.appendChild(card);
+    });
+}
+
+// Chamada da função para listar os repositórios
+listRepo();
+
+
+// scroll carrosel
